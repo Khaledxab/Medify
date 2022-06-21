3780c14aa3562491b67e1b53bf3c4e2ea760d315
khaledxab <khaledxab@gmail.com>
2022-06-21 21:03:35 +0100

    ADD onPush when login  😁

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:t_helper/screens/home_screen.dart';
 import 'package:t_helper/screens/registration_screen.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -63,7 +64,10 @@ class _LoginScreenState extends State<LoginScreen> {
       child: MaterialButton(
           padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
           minWidth: MediaQuery.of(context).size.width,
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => HomeScreen()));
+          },
           child: Text("Login",
               textAlign: TextAlign.center,
               style: TextStyle(
