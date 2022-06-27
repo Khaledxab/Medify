391660c1bc9d29b8258066e6d557ad7847d75e58
khaledxab <khaledxab@gmail.com>
2022-06-27 02:21:18 +0100

    add Maps (FIRST TEST) 🔥

--- lib/screens/location.dart
+++ lib/screens/location.dart
@@ -14,21 +14,21 @@ class _LocationScreenState extends State<LocationScreen> {
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(36.831749, 10.232919),
     zoom: 14.4746,
   );
 
   static final CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
+      target: LatLng(36.833145010736175, 10.23751249202125),
+      tilt: 1,
       zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.normal,
+        mapType: MapType.hybrid,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
